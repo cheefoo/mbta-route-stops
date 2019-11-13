c82757355c2d062870b0be1c0de8edcb00002e4b
Tayo Olajide <tayoolajide@Tayos-Air.home>
2019-11-13 06:34:51 -0600

    changes to second answer

--- src/main/java/com/ty/fm/MbtaRequestApp.java
+++ src/main/java/com/ty/fm/MbtaRequestApp.java
@@ -11,7 +11,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.List;
+import java.util.*;
 
 
 //curl -X GET "https://api-v3.mbta.com/routes?page%5Boffset%5D=0&page%5Blimit%5D=10&sort=-long_name" -H "accept: application/vnd.api+json"
@@ -24,14 +24,62 @@ public class MbtaRequestApp
     {
 
         List<RouteRequest> routeRequestList = callRouteAPI("https://api-v3.mbta.com/routes?");
+        Map<String, Integer> map = new HashMap();
+        Map<String, List<RouteStop>> routeStopMap;
+        List<Map<String, List<RouteStop>>> allRouteStopList = new ArrayList<>();
+        List<String> routeNameList = new ArrayList<>();
         for(RouteRequest routeRequest: routeRequestList)
         {
             System.out.println(routeRequest.getRouteRequestAttribute().getLong_name());
-            //System.out.println(routeRequest.getRouteRequestAttribute().getDirection_destinations().length);
+            routeNameList.add(routeRequest.getId());
+            routeStopMap = callStopAPI("https://api-v3.mbta.com/stops?", routeRequest.getId());
+            allRouteStopList.add(routeStopMap);
+            map.put(routeRequest.getRouteRequestAttribute().getLong_name(),routeStopMap.get(routeRequest.getId()).size());
+            System.out.println("Size of  " + routeRequest.getId() + " is " + routeStopMap.get(routeRequest.getId()).size());
         }
-        List<RouteStop> routeStopList = callStopAPI("https://api-v3.mbta.com/stops?", "Orange");
-        System.out.println("Size is " + routeStopList.size());
+        System.out.println("Size of routeNames is " + routeNameList.size());
+        System.out.println("Size of allRouteStopList is " + allRouteStopList.size());
+        Map.Entry maxValueEntry = maxUsingCollectionsMaxAndLambda(map);
+        System.out.println("Subway route with most number of stops is " + maxValueEntry.getKey() + " " + "  and MaxValue is " + maxValueEntry.getValue());
 
+        Map.Entry minValueEntry = minUsingCollectionsMaxAndLambda(map);
+        System.out.println("Subway route with fewest number of stops is " + minValueEntry.getKey() + " " + "  and MinValue is " + minValueEntry.getValue());
+
+        Map<String, Integer>  routeStopListMap = new HashMap<String, Integer>();
+        for(int j=0; j<routeNameList.size(); j++)
+        {
+            List list = allRouteStopList.get(j).get(routeNameList.get(j));
+        }
+
+        //two stops
+
+
+        //three stops
+
+
+
+        /*for(int i = 0; i <  allRouteStopList.size(); i++)
+        {
+            int count = routeStopListMap.containsKey(routeStop.getRouteStopAttribute().getName()) ? routeStopMap.get(routeStop.getRouteStopAttribute().getName()) : 0;
+            routeStopMap.put(routeStop.getRouteStopAttribute().getName(), count+1);
+            System.out.println("Route Stop Id  is " + routeStop.getId() +  " : Stop Name for  " + routeStop.getRouteStopAttribute().getName());
+        }
+
+        Map.Entry maxValue = maxUsingCollectionsMaxAndLambda(routeStopMap);
+        System.out.println("Subway route with fewest number of stops is " + maxValue.getKey() + " " + "  and MinValue is " + maxValue.getValue());*/
+
+    }
+
+    public static <K, V extends Comparable<V>> Map.Entry<K, V> maxUsingCollectionsMaxAndLambda(Map<K, V> map) {
+        Map.Entry<K, V> maxEntry = Collections.max(map.entrySet(), (Map.Entry<K, V> e1, Map.Entry<K, V> e2) -> e1.getValue()
+                .compareTo(e2.getValue()));
+        return maxEntry;
+    }
+
+    public static <K, V extends Comparable<V>> Map.Entry<K, V> minUsingCollectionsMaxAndLambda(Map<K, V> map) {
+        Map.Entry<K, V> minEntry = Collections.min(map.entrySet(), (Map.Entry<K, V> e1, Map.Entry<K, V> e2) -> e1.getValue()
+                .compareTo(e2.getValue()));
+        return minEntry;
     }
 
     private static List<RouteRequest> callRouteAPI(String urlString) throws IOException
@@ -42,7 +90,7 @@ public class MbtaRequestApp
         //URL url = new URL("https://api-v3.mbta.com/routes");
         URL url = new URL(urlString+query);
 
-        System.out.println("URL is " + url);
+        //System.out.println("URL is " + url);
 
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
         //con.setRequestMethod("POST");
@@ -65,14 +113,14 @@ public class MbtaRequestApp
             {
                 response.append(responseLine.trim());
                 //InputStream targetStream = new ByteArrayInputStream(responseLine.getBytes());
-                System.out.println(responseLine.trim() + "\n");
+                //System.out.println(responseLine.trim() + "\n");
 
             }
             ObjectMapper mapper = new ObjectMapper();
 
             TypeReference<RouteRequestList> mapType = new TypeReference<RouteRequestList>() {};
             routeRequestList = mapper.readValue(response.toString(), mapType);
-            System.out.println("Size is " + routeRequestList.getRouteRequestList().size());
+            //System.out.println("Size is " + routeRequestList.getRouteRequestList().size());
 
         }
 
@@ -80,15 +128,16 @@ public class MbtaRequestApp
     }
 
     //curl -X GET "https://api-v3.mbta.com/stops?filter%5Broute%5D=Orange" -H "accept: application/vnd.api+json"
-    private static List<RouteStop> callStopAPI(String urlString, String stop) throws IOException
+    private static Map<String, List<RouteStop>> callStopAPI(String urlString, String stop) throws IOException
     {
+        Map <String, List<RouteStop>> map = new HashMap<>();
         String query = String.format("filter[route]=%s",
                 URLEncoder.encode(stop, "UTF-8"));
 
         //URL url = new URL("https://api-v3.mbta.com/routes");
         URL url = new URL(urlString+query);
 
-        System.out.println("URL is " + url);
+        //System.out.println("URL is " + url);
 
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
         //con.setRequestMethod("POST");
@@ -111,18 +160,18 @@ public class MbtaRequestApp
             {
                 response.append(responseLine.trim());
                 //InputStream targetStream = new ByteArrayInputStream(responseLine.getBytes());
-                System.out.println(responseLine.trim() + "\n");
+                //System.out.println(responseLine.trim() + "\n");
 
             }
             ObjectMapper mapper = new ObjectMapper();
 
             TypeReference<RouteStops> mapType = new TypeReference<RouteStops>() {};
             routeStopsList = mapper.readValue(response.toString(), mapType);
-            System.out.println("Size is " + routeStopsList.getRouteStopList().size());
+            //System.out.println("Size is " + routeStopsList.getRouteStopList().size());
 
         }
-
-        return routeStopsList.getRouteStopList();
+        map.put(stop,routeStopsList.getRouteStopList());
+        return map;
     }
 
 
